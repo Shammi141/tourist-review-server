0da6ec896456a1e8e0caaaf7701139fcd6d1f634
Shammi141 <jannatulshammi181@gmail.com>
2022-11-09 16:51:28 +0600

    service details api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +13,34 @@ app.use(express.json());
 //mongodb information
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yjnxy2v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('tourReview').collection('services');
+
+        //creating api for loading services data
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        //services api for service details
+        app.get('/services/:id', async(req, res) =>{
+            const id= req.params.id;
+            const query = {_id:ObjectId(id)};
+            const services = await serviceCollection.findOne(query);
+            console.log(services);
+            res.send(services);
+        });
+    }
+    finally{
+
+    }
+}
+
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) =>{
     res.send('Tourist review server is running');
